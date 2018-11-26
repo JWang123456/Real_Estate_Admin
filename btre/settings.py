577b3fbd6bc2b98902f39dffc7426469df8fa398
JWang123456 <wanggaojie913@yahoo.com>
2018-11-25 22:06:34 -0600

    M

--- btre/settings.py
+++ btre/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'btre/static')
+# ]
